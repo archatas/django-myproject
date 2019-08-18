3869c1c152b4711a93e96edaf0dce3704eb6f9be
Aidas Bendoraitis <aidasbend@yahoo.com>
2019-08-18 10:45:49 +0200

    ⚙️ LOCALE_PATHS setting added.

--- myproject/settings/_base.py
+++ myproject/settings/_base.py
@@ -146,6 +146,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale'),
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
