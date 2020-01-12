684f0aa8aa154a3f9b375e0192543974167a7bfe
Aidas Bendoraitis <aidasbend@yahoo.com>
2020-01-12 11:01:13 +0100

    ⚙️ Wsgi files for staging and productions environments.

--- myproject/wsgi_staging.py
+++ myproject/wsgi_staging.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings.staging')
 
 application = get_wsgi_application()
